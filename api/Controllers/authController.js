34a82c467a748910ea1b102476ada053b0be39c5
HarshitDevsainia <hdevsania.in>
2024-05-23 17:51:14 +0530

    Create the Signin Route

--- api/Controllers/authController.js
+++ api/Controllers/authController.js
@@ -1,6 +1,7 @@
 import {errorHandler} from '../utils/error.js';
 import User from'../model/user.js';
 import bcrypt from 'bcrypt';
+
 export const signUp=async(req,res,next)=>{
     try{
         const {username,email,password}=req.body;
@@ -14,6 +15,28 @@ export const signUp=async(req,res,next)=>{
         res.send({JSON,msg:'SuccessFully SignUp'});
     }
     catch(err){
-        next(err);
+        return next(err);
+    }
+}
+export const signIn=async(req,res,next)=>{
+    const {email,password}=req.body;
+    if(!email || !password || email==='' || password===''){
+        next(errorHandler(400,'required all information'));
+    }
+    try{
+        const currUser=await User.find({email:email});
+        if(!currUser[0]){
+            next(errorHandler(400,'User Not Found'));
+            return;
+        }
+        const checkPass=await bcrypt.compareSync(password,currUser[0].password);
+        if(!checkPass){
+            next(errorHandler(400,'Incorrect Password'));
+            return;
+        }
+        res.send({JSON,msg: "Successfully login"});
+    }
+    catch(err){
+       return next(err);
     }
 }
--- api/routes/authRoute.js
+++ api/routes/authRoute.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { signUp } from '../Controllers/authController.js';
+import { signIn, signUp } from '../Controllers/authController.js';
 
 const router=express.Router();
 
 router.post('/signUp',signUp);
+router.post('/signIn',signIn)
 
 export default router;
