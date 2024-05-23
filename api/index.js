5b0e920b15bfecdcff7dcaa8ef1dff33058413f8
HarshitDevsainia <hdevsania.in>
2024-05-23 12:55:54 +0530

    Connent the DataBase

--- api/index.js
+++ api/index.js
@@ -1,6 +1,15 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
 const app=express();
 
+mongoose.connect(process.env.MONGO_URL).then(()=>{
+    console.log("DataBase is Connected");
+}).catch((err)=>{
+    console.log(err);
+})
 app.listen(8080,()=>{
     console.log("App is Listen to the Port 8080");
 })
