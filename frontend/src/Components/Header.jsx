b62dd3ac42a1d5bab5b0f5c0c27dac8e1d4f08e1
HarshitDevsainia <hdevsania.in>
2024-05-24 18:18:17 +0530

    fix the button issue

--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -30,19 +30,22 @@ export default function Header() {
     <>
         <Navbar>
             <Navbar.Brand>
+               <Link to={'/'} className='flex'>
                 <img className='mr-3 h-6 sm:h-9 rounded-full' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvKC3BoSUy5kUs8iCVQOh5OvEKrP2xM1y-rg&s" alt="logo" />
                 <span className=' self-center whitespace-nowrap text-xl font-semibold'>Top Events</span>
+               </Link>
             </Navbar.Brand>
             <Navbar.Toggle/>
             <Navbar.Collapse>
                 <Navbar.Link href='/' active={path==='/'}>Home</Navbar.Link>
                 <Navbar.Link href='/about' active={path==='/about'}>About</Navbar.Link>
                 <Navbar.Link href='/create' active={path==='/create'}>Create Event</Navbar.Link>
+                {(currUser==null)?
+                <Navbar.Link href='/signIn' className='hover:text-blue-500' >SignIn</Navbar.Link>
+                :<p className='hover:text-blue-500 pl-3 cursor-pointer' onClick={handleSignOut}>LogOut</p>
+                }
             </Navbar.Collapse>
-            {(currUser==null)?
-                <Link to={'/signIn'}><Button size={'sm'} outline className=' !bg-violet-900'>SignIn</Button></Link>
-                :<Button size={'sm'} outline className=' !bg-violet-900' onClick={handleSignOut}>LogOut</Button>
-            }
+            
         </Navbar>
     </>
   )
